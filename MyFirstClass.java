4a90a09dd481037c8eba12e4b03ad7ea9dbc11a3
DavidFriedl2047 <s51729@edu.campus02.at>
2022-09-23 17:10:23 +0200

    My First Class created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    public static void main(String[] args) {
+        //wir haben unsere erste Klasse erzeugt
+        //psvm
+        //erste Ausgabe
+
+        System.out.println("Ich werde auf der Konsole angezeigt");
+        //args
+        System.out.println(Arrays.toString(args));
+
+    }
+}
